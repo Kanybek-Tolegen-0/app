50fd5890d444f8f1f9d0ed6826d59b0430ffa66d
Kanybek <kanybektlgn0@gmail.com>
2021-12-05 20:16:10 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,135 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="product-list">
+      <Product v-for="product in products" :key="product.name" :product="product"/>
+    </div>
+
+    <div class="hotbuttons">
+      <input ref="title" class="product-value" type="text"/>
+      <input ref="price" class="product-value" type="text"/>
+      <button class="HotButton add" v-on:click = "add">Создать</button>
+      <button class="HotButton delete" v-on:click = "remove">Удалить</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Product from '../src/components/Product.vue';
+import axios from 'axios';
 
 export default {
   name: 'App',
+  data() {
+    return (
+      {
+        products: {},
+      }
+    );
+  },
   components: {
-    HelloWorld
+    Product,
+  },
+  beforeMount() {
+    axios.get('https://project-b156e-default-rtdb.firebaseio.com/products.json')
+    .then((response) => {
+      this.products = response.data;
+      // const keys = Object.keys((this.products));
+      for(let k in this.products) {
+        this.products[k].id = k;
+      }
+  });
+
+  },
+  methods: {
+    async add() {
+      await axios.post('https://project-b156e-default-rtdb.firebaseio.com/products.json', {
+        title: this.$refs.title.value,
+        price: +this.$refs.price.value,
+      }).then((response) => console.log(response))
+      .catch((err) => console.log(err));
+
+      await axios.get('https://project-b156e-default-rtdb.firebaseio.com/products.json')
+      .then((response) => {
+        this.products = response.data;
+        for(let k in this.products) {
+        this.products[k].id = k;
+        }
+      });
+    },
+
+    async remove() {
+      let target = null;
+      for( let k in this.products) {
+        if(this.products[k].title === this.$refs.title.value && this.products[k].price === +this.$refs.price.value) {
+          target = this.products[k].id;
+          await axios.delete(`https://project-b156e-default-rtdb.firebaseio.com/products/${target}.json`);
+        }
+      }
+
+      await axios.get('https://project-b156e-default-rtdb.firebaseio.com/products.json')
+          .then((response) => {
+          this.products = response.data;
+          for(let k in this.products) {
+          this.products[k].id = k;
+        }
+      });
+    },
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: sans-serif;
+  }
+
+  .product-list {
+    background-color: gray;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+  }
+
+  .HotButton {
+    width: 200px;
+    height: 60px;
+    font-size: 24px;
+    background: none;
+    border-radius: 8px;
+    cursor: pointer;
+  }
+
+  .HotButton:hover {
+    border: none;
+    font-size: 28px;
+    color: white;
+    transition: 0.2s all linear;
+  }
+
+  .add:hover {
+    background: green;
+  }
+
+  .delete:hover {
+    background: red;
+  }
+
+  .hotbuttons {
+    margin: 24px;
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+  }
+
+  .product-value {
+    width: 320px;
+    font-size: 32px;
+    height: 60px;
+    border-radius: 16px;
+    outline: none;
+    text-indent: 16px;
+  }
 </style>
---
+++ src/components/Product.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="product">
+        <h1>{{product.title}}</h1>
+        <p>Цена: {{product.price}}</p>
+        <p>id: {{product.id}}</p>
+    </div>
+</template>
+
+<script>
+export default ({
+    name: "Product",
+    props: {
+       product: Object,
+    },
+})
+</script>
+
+<style>
+    .product {
+        text-align: center;        
+    }
+</style>
